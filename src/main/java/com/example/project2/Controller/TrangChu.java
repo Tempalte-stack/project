11b1a9ea61c97c08e5dd4d8a3006bc246b833faa
DELL <ij290704@gmail.com>
2024-08-29 19:57:59 +0700

    hi

---
+++ src/main/java/com/example/project2/Controller/TrangChu.java
@@ -0,0 +1,18 @@
+package com.example.project2.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+@Controller
+public class TrangChu {
+    @GetMapping("/trangchu")
+    public String home(Model model) {
+        return "TrangChu/trangchu";
+    }
+
+
+}
+
--- src/main/java/com/example/project2/Model/HoaDon.java
+++ src/main/java/com/example/project2/Model/HoaDon.java
@@ -1,4 +1,25 @@
 package com.example.project2.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hoadDon")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class HoaDon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idHD")
+    private Integer idhd;
+    @Column(name = "idNguoiDung")
+    private String idnguoidung;
+    @Column(name = "ngaylap")
+    private String ngaylap;
+
 }
